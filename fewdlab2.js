7466750155854c915388252ffb5e408ed00d77d1
EinarV77 <einar.vallejo77@gmail.com>
2020-02-17 23:50:13 +0000

    commit

---
+++ fewdlab2.js
@@ -0,0 +1,22 @@
+var myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
+    console.log(myAlphabet.length);
+
+function myAlphabetlength(){return myAlphabet.length};
+    
+let planets = ['Mercury','Venus','Earth','Mars','Jupiter']
+console.log(planets[0])  
+console.log(planets[1])
+console.log(planets[2])
+console.log(planets[3])
+console.log(planets[4])
+
+let wowDatatypes = ['Einar', 1111,true ]
+console.log(wowDatatypes[0])  
+console.log(wowDatatypes[1])
+console.log(wowDatatypes[2])
+
+var hello = function (){
+    return 'Hello world!'
+}
+var result = hello();
+console.log(hello);
